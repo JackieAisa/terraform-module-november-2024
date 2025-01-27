237d9d5859991497557a4d302731054c84edda58
JackieAisa <aisakhunova@gmail.com>
2025-01-27 23:15:25 +0000

    igw

--- main.tf
+++ main.tf
@@ -9,6 +9,11 @@ resource aws_vpc main {
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.subnet_cidr
+}
 
-
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = var.igw_name
+  }
 }
